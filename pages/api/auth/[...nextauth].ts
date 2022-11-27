a8cdb0daa866b4f034ac7bd15cca07b3ba8c725c
nonwiz <nonwiz@pm.me>
2022-11-28 06:56:06 +0700

    Fix undefined secret of authOptions

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -32,7 +32,7 @@ const callbacks = {
 
 }
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers,
   callbacks,
